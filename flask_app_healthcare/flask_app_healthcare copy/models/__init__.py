3c6e76a67495166487b0b485bd6f55f080cafa43
Renan Cz <renanczervinski@hotmail.com>
2024-12-11 13:42:28 -0300

    dega

--- flask_app_healthcare/flask_app_healthcare copy/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .actuators import Actuator
-from .devices import Device
-from .historic import Historic
-from .kits import Kit
-from .sensors import Sensor
-from .users import User
--- flask_app_healthcare/flask_app_healthcare copy/static/js/home.js
+++
@@ -1,5 +0,0 @@
-const capacityLevel = document.querySelector(".capacity-level");
-capacityLevel.style.height = parseInt(document.getElementById("peopleInfo").innerText) + "%";
-
-const temperatureLevel = document.querySelector(".temperature-level");
-temperatureLevel.style.height = parseInt(document.getElementById("temperatureInfo").innerText) + "%";
